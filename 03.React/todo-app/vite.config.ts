f850d904be1c1ec6eeb909c3db0f9ec16f6f3da0
Suam Kang <ksa199653@gmail.com>
2023-11-19 18:59:42 +0900

    chore: 상대경로 오류 수정

--- 03.React/todo-app/vite.config.ts
+++ 03.React/todo-app/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@apis": path.resolve(__dirname, "./src/apis"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@layout": path.resolve(__dirname, "./src/layout"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@store": path.resolve(__dirname, "./src/store"),
+    },
+  },
+});
